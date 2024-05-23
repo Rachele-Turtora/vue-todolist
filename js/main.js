014997e226a7ee88963f0c4d20f13d3ddf4adea0
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-23 12:11:07 +0200

    [FIX] proofreading

--- js/main.js
+++ js/main.js
@@ -25,26 +25,37 @@ const { createApp } = Vue
                 done: false
             }
         ],
-        newTask: ""
+        newTask: "",
+        findTask: null
       }
     },
     methods: {
         removeTask(index){
-            this.todo.splice(index, 1);
+            //this.todo.splice(index, 1);
+            this.todo = this.todo.filter((_, i) => i !== index);
         },
 
         addTask(){
-            if (this.newTask.length){
-                this.todo.push({
-                    text: this.newTask,
-                    done: false
-                });
-            }
+            if (!this.newTask) return;
+            this.todo.push({
+                text: this.newTask.trim(),  // toglie spazi prima e dopo
+                done: false
+            });
             this.newTask = "";
         },
         
-        done(index){
+        toggle(index){
             this.todo[index].done = !this.todo[index].done;
+        },
+
+        searchTask(){
+            if (this.findTask){
+                return this.todo.filter((element) => {
+                    return element.text.toLowerCase().includes(this.findTask.toLowerCase())
+                })
+            } else {
+                return this.todo
+            }
         }
     }
   }).mount('#app')
